80bff5a08bcbe3e28cd6c88ef6421fb930b31998
Leyland Yang <yangleyland@gmail.com>
2025-10-14 22:51:39 -0700

    update color

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.className}`}>
-      <body className="antialiased tracking-tight">
-        <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 dark:bg-zinc-950 bg-white text-gray-900 dark:text-zinc-200">
+      <body className="antialiased tracking-tight bg-white dark:bg-zinc-950">
+        <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 text-gray-900 dark:text-zinc-200">
           <main className="max-w-[60ch] mx-auto w-full space-y-6">
             <ViewTransition name="test">{children}</ViewTransition>
           </main>
